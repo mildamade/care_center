15ae8fa6b6b1b9068b309c80db87081aebfa5ba4
Graeme Gellatly <gdgellatly@gmail.com>
2018-07-20 14:43:14 +1200

    [FIX] Files not renamed on relicensing to lgpl

--- support_team/__manifest__.py
+++ support_team/__manifest__.py
@@ -1,5 +1,5 @@
 # © 2017 Dave Burkholder <dave@thinkwelldesigns.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
     'name': 'Support Teams',
